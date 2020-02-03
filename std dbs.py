d5e8542953cbace040631eca341ab63995cd1952
TechAkhil <43501564+techakhilc47@users.noreply.github.com>
2020-02-04 03:18:51 +0530

    Update std dbs.py
    Add dark theme and hover fx

--- std dbs.py
+++ std dbs.py
@@ -11,6 +11,7 @@ import tkinter.messagebox
 import inspect
 import std_dbs
 
+
 class Student:
     def __init__(self,root):
         self.root = root
@@ -18,7 +19,7 @@ class Student:
         self.root.iconbitmap('Student.ico') 
         self.root.geometry('1350x700+0+0')
         self.root.config(bg='cadet blue')
-        
+        self.d=0
         StdID = StringVar()
         Firstname = StringVar()
         Surname = StringVar()
@@ -52,10 +53,76 @@ class Student:
 
         def darktheme():
             global xs
-            xs = inspect.sys._getframe(1)
-            print(xs)
-            pass
-
+            if self.d == 0 :
+                self.root.config(bg='#202020')
+                MainFrame.config(bg='#202020')
+                TitleFrame.config(bg='#181818')
+                self.lblTitle.config(bg='#181818', fg='#ABCDEF')
+                ButtonFrame.config(bg='#181818')
+                DataFrame.config(bg='#181818')
+                DataFrameLEFT.config(bg='#181818', fg='ghost white')
+                DataFrameRIGHT.config(bg='#181818',fg='ghost white')
+                self.lblStdID.config(bg='#181818',fg='ghost white')
+                self.txtStdID.config(bg='#505050', fg='ghost white')
+                self.lblfna.config(bg='#181818',fg='ghost white')
+                self.txtfna.config(bg='#505050', fg='ghost white')
+                self.lblsna.config(bg='#181818',fg='ghost white')
+                self.txtsna.config(bg='#505050', fg='ghost white')
+                self.lblDoB.config(bg='#181818',fg='ghost white')
+                self.txtDoB.config(bg='#505050', fg='ghost white')
+                self.lblage.config(bg='#181818',fg='ghost white')
+                self.txtage.config(bg='#505050', fg='ghost white')
+                self.lblgen.config(bg='#181818',fg='ghost white')
+                self.txtgen.config(bg='#505050', fg='ghost white')
+                self.lbladr.config(bg='#181818',fg='ghost white')
+                self.txtadr.config(bg='#505050', fg='ghost white')
+                self.lblMobile.config(bg='#181818',fg='ghost white')
+                self.txtMobile.config(bg='#505050', fg='ghost white')
+                self.btndarktheme.config(text='Light theme',bg='#101010',fg='ghost white')
+                self.btnAddData.config(bg='#101010',fg='ghost white')
+                self.btnDisplayData.config(bg='#101010',fg='ghost white')
+                self.btnClearData.config(bg='#101010',fg='ghost white')
+                self.btnSearchData.config(bg='#101010',fg='ghost white')
+                self.btnDeleteData.config(bg='#101010',fg='ghost white')
+                self.btnUpdateData.config(bg='#101010',fg='ghost white')
+                self.btnExit.config(bg='#101010',fg='ghost white')
+                studentlist.config(bg='#505050', fg='ghost white')
+                self.d += 1
+            else:
+                self.root.config(bg='cadet blue')
+                MainFrame.config(bg='cadet blue')
+                TitleFrame.config(bg='ghost white')
+                self.lblTitle.config(bg='white', fg='black')
+                ButtonFrame.config(bg='ghost white')
+                DataFrame.config(bg='cadet blue')
+                DataFrameLEFT.config(bg='ghost white', fg='black')
+                DataFrameRIGHT.config(bg='ghost white', fg='black')
+                self.lblStdID.config(bg='ghost white', fg='black')
+                self.txtStdID.config(bg='white', fg='black')
+                self.lblfna.config(bg='ghost white', fg='black')
+                self.txtfna.config(bg='white', fg='black')
+                self.lblsna.config(bg='ghost white', fg='black')
+                self.txtsna.config(bg='white', fg='black')
+                self.lblDoB.config(bg='ghost white', fg='black')
+                self.txtDoB.config(bg='white', fg='black')
+                self.lblage.config(bg='ghost white', fg='black')
+                self.txtage.config(bg='white', fg='black')
+                self.lblgen.config(bg='ghost white', fg='black')
+                self.txtgen.config(bg='white', fg='black')
+                self.lbladr.config(bg='ghost white', fg='black')
+                self.txtadr.config(bg='white', fg='black')
+                self.lblMobile.config(bg='ghost white', fg='black')
+                self.txtMobile.config(bg='white', fg='black')
+                self.btndarktheme.config(text='Dark theme',bg='ghost white',fg='black')
+                self.btnAddData.config(bg='ghost white',fg='black')
+                self.btnDisplayData.config(bg='ghost white',fg='black')
+                self.btnClearData.config(bg='ghost white',fg='black')
+                self.btnSearchData.config(bg='ghost white',fg='black')
+                self.btnDeleteData.config(bg='ghost white',fg='black')
+                self.btnUpdateData.config(bg='ghost white',fg='black')
+                self.btnExit.config(bg='ghost white',fg='black')
+                studentlist.config(bg='white', fg='black')
+                self.d -= 1
 
 
         def iExit():
@@ -91,7 +158,7 @@ class Student:
             studentlist.delete(0,END)
             for row in std_dbs.searchData(StdID.get(),Firstname.get(),Surname.get(),DoB.get(),Age.get(),Gender.get(), Address_.get(),Mobile.get()):
                 studentlist.insert(END,row,str(""))
-            
+
         def update():
             if(len(StdID.get())!=0):
                 std_dbs.deleteRec(sd[0])
@@ -99,19 +166,25 @@ class Student:
                 std_dbs.addStdRec(StdID.get(),Firstname.get(),Surname.get(),DoB.get(),Age.get(),Gender.get(),Address_.get(),Mobile.get())
                 studentlist.delete(0,END)
                 studentlist.insert(END,(StdID.get() , Firstname.get() , Surname.get(),DoB.get(),Age.get(),Gender.get(),Address_.get(),Mobile.get()))
-       
+            DisplayData()
         def addData():
             if (len(StdID.get())!=0):
                 std_dbs.addStdRec(StdID.get(), Firstname.get(), Surname.get(), DoB.get(), Age.get(), Gender.get(), Address_.get(), Mobile.get())
                 studentlist.delete(0,END)
                 studentlist.insert(END,(StdID.get(),Firstname.get(),Surname.get(),DoB.get(),Age.get(), Gender.get(),Address_.get(),Mobile.get()))
-                    
+            DisplayData()
             
-        def entered(event):
-            self.btnAddData.config(bg='#343434', fg='#ffffff')
+        def entered(event, btn):
+            if self.d == 0:
+                exec(str(btn)+".config(bg='#a6a6a6', fg='ghost white')")
+            if self.d == 1:
+                exec(str(btn)+".config(bg='#a6a6a6', fg='black')")
 
-        def left(event):
-            self.btnAddData.config(bg='#ffffff', fg='#000000')
+        def left(event, btn):
+            if self.d==1:
+                exec(str(btn)+".config(bg='#101010', fg='ghost white')")
+            if self.d == 0:
+                exec(str(btn)+".config(bg='ghost white', fg='black')")
 
 
 
@@ -127,10 +200,10 @@ class Student:
         self.lblTitle = Label(TitleFrame, font=('arial',47,'bold'),text='Student Database Management System')
         self.lblTitle.grid(sticky=W)
 
-        ButtonFrame =Frame(MainFrame, bd=2, width=1350, height=70, padx=18,pady=10, bg="Ghost White", relief=RIDGE)
+        ButtonFrame =Frame(MainFrame, bd=1, width=1350, height=70, padx=40, pady=10, bg="Ghost White", relief=RIDGE)
         ButtonFrame.pack(side=BOTTOM)
 
-        DataFrame =Frame(MainFrame, bd=1, width=1350, height=400, padx=40, pady=20, bg='cadet blue', relief=RIDGE )
+        DataFrame =Frame(MainFrame, bd=2, width=1350, height=40, padx=40, pady=10, bg='cadet blue', relief=RIDGE)
         DataFrame.pack(side=BOTTOM)
 
         DataFrameLEFT =LabelFrame(DataFrame, bd=1, width=1000, height=600, padx=20, relief=RIDGE , font=('arial',20,'bold'), text='Student Information', bg='Ghost White')
@@ -194,30 +267,44 @@ class Student:
         #===================================Button Widget===================================#===================================
         self.btnAddData = Button(ButtonFrame, text="Add new",font=('arial',20,'bold'),height=1,width=10, bd=4, command= addData)
         self.btnAddData.grid(row=0,column=0)
-        self.btnAddData.bind('<Enter>', entered)
-        self.btnAddData.bind("<Leave>", left)
+        self.btnAddData.bind('<Enter>', lambda event :entered(event, 'self.btnAddData'))
+        self.btnAddData.bind('<Leave>', lambda event :left(event, 'self.btnAddData'))
      
         self.btnDisplayData = Button(ButtonFrame, text="Display",font=('arial',20,'bold'),height=1,width=10, bd=4,command=DisplayData)
         self.btnDisplayData.grid(row=0,column=1)
-        
+        self.btnDisplayData.bind('<Enter>', lambda event: entered(event, 'self.btnDisplayData'))
+        self.btnDisplayData.bind('<Leave>', lambda event: left(event, 'self.btnDisplayData'))
+
         self.btnClearData = Button(ButtonFrame, text="Clear ",font=('arial',20,'bold'),height=1,width=10, bd=4, command= clearData)
         self.btnClearData.grid(row=0,column=2)
-        
+        self.btnClearData.bind('<Enter>', lambda event: entered(event, 'self.btnClearData'))
+        self.btnClearData.bind('<Leave>', lambda event: left(event, 'self.btnClearData'))
+
         self.btnSearchData = Button(ButtonFrame, text="Search",font=('arial',20,'bold'),height=1,width=10, bd=4, command= searchDatabase)
         self.btnSearchData.grid(row=0,column=3)
+        self.btnSearchData.bind('<Enter>', lambda event: entered(event, 'self.btnSearchData'))
+        self.btnSearchData.bind('<Leave>', lambda event: left(event, 'self.btnSearchData'))
+
         
         self.btnDeleteData = Button(ButtonFrame, text="Delete",font=('arial',20,'bold'),height=1,width=10, bd=4,command= DeleteData)
         self.btnDeleteData.grid(row=0,column=4)
-        
+        self.btnDeleteData.bind('<Enter>', lambda event: entered(event, 'self.btnDeleteData'))
+        self.btnDeleteData.bind('<Leave>', lambda event: left(event, 'self.btnDeleteData'))
+
         self.btnUpdateData= Button(ButtonFrame, text="Update",font=('arial',20,'bold'),height=1,width=10, bd=4,command= update)
         self.btnUpdateData.grid(row=0,column=5)
-        
+        self.btnUpdateData.bind('<Enter>', lambda event: entered(event, 'self.btnUpdateData'))
+        self.btnUpdateData.bind('<Leave>', lambda event: left(event, 'self.btnUpdateData'))
+
         self.btnExit = Button(ButtonFrame, text="Exit",font=('arial',20,'bold'),height=1,width=10, bd=4, command = iExit)
         self.btnExit.grid(row=0,column=6)
+        self.btnExit.bind('<Enter>', lambda event: entered(event, 'self.btnExit'))
+        self.btnExit.bind('<Leave>', lambda event: left(event, 'self.btnExit'))
 
         self.btndarktheme = Button(MainFrame, text="Dark Theme",font=('arial',20,'bold'), command = darktheme)
         self.btndarktheme.pack()
-        
+        self.btndarktheme.bind('<Enter>', lambda event: entered(event, 'self.btndarktheme'))
+        self.btndarktheme.bind('<Leave>', lambda event: left(event, 'self.btndarktheme'))
 
 if __name__=='__main__':
     root = Tk()
